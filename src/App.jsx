87d3de76da72d2845a3dc457a5cbae862fd5fb3e
Rafa Casariego <raff9530@gmail.com>
2025-04-01 19:30:48 +0200

    Proyecto inicial con Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  return (
+    <h1 className="text-red-500 text-3xl font-bold underline">
+      ¡Tailwind funciona!
+    </h1>
+  );
+}
+
+
+export default App;
+
